0d6e6534feb602c9dd4476e046df13a906f88295
RScodes1 <raj19all@gmail.com>
2025-12-24 11:22:59 +0530

    frontend deployed link giving cors error

--- backend 2/main.py
+++ backend 2/main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://vectorshift-two.vercel.app"],
+    allow_origins=["http://localhost:3000", "https://vectorshift-two.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
